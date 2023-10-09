fac3ccdf70676e9309154370aca431683e7b5a80
Greg <kimanthigregory>
2023-10-09 22:13:15 +0300

    perfected the animation but the code still needs refoctoring

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-var shareIcon = document.querySelector(".share-icon-container");
-shareIcon.addEventListener("click", function() {
-    document.querySelector(".social-media-icons-container").classList.toggle("show-pop-up");
-    shareIcon.style.backgroundColor = " hsl(217, 19%, 35%)";
+const bigSCreenShareIcon = document.querySelector(".share-icon-container");
+let animateSmallScreenShareIcon = document.querySelector(".animate-share-icon");
+let smallScreenShareIcon =document.querySelector(".share-icon-container-sm");
+let Buttonclicked = false;
+bigSCreenShareIcon.addEventListener("click", function() {
+    bigScreenAnimation();
 })
+animateSmallScreenShareIcon.addEventListener("click", function(){
+    document.querySelector(".share-animation").classList.add("remove-slide-animation");
+    bigScreenAnimation();
+})
+smallScreenShareIcon.addEventListener("click", function(){
+    // bigScreenAnimation();
+    document.querySelector(".share-animation").classList.remove("remove-slide-animation");
+    document.querySelector(".share-animation").classList.add("show-slide-animation");
+
+})
+function bigScreenAnimation() {
+    if (Buttonclicked !==true) {
+        document.querySelector(".social-media-icons-container").classList.remove("remove-pop-up");
+        document.querySelector(".social-media-icons-container").classList.add("show-pop-up");
+        bigSCreenShareIcon.style.backgroundColor = " hsl(217, 19%, 35%)";
+        document.querySelector(".icon path").style.fill="white";
+        Buttonclicked = true;
+    }
+    else{
+        document.querySelector(".social-media-icons-container").classList.add("remove-pop-up");
+        document.querySelector(".icon path").style.fill=" hsl(214, 17%, 51%)";
+        bigSCreenShareIcon.style.backgroundColor = "  hsl(210, 46%, 95%)";
+        Buttonclicked = false;
+    }
+}
+
+function smallScreenAnimation() {
+    
+}
